6a0333402e8909e6370f86c36bf12442e43e5635
David Giesberg <git@davidgiesberg.net>
2014-03-02 15:31:51 -0600

    Emacs: Disable arrow keys and add markdown-mode

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -3,6 +3,7 @@
 
 (add-to-list 'load-path "~/.emacs.d/themes/emacs-color-theme-solarized")
 (add-to-list 'custom-theme-load-path "~/.emacs.d/themes/emacs-color-theme-solarized")
+(add-to-list 'load-path "~/.emacs.d/")
 
 (load-theme 'solarized-light t)
 
@@ -19,3 +20,21 @@
       `((".*" . ,temporary-file-directory)))
 (setq auto-save-file-name-transforms
       `((".*" ,temporary-file-directory t)))
+
+(autoload 'markdown-mode "markdown-mode"
+   "Major mode for editing Markdown files" t)
+(add-to-list 'auto-mode-alist '("\\.text\\'" . markdown-mode))
+(add-to-list 'auto-mode-alist '("\\.markdown\\'" . markdown-mode))
+(add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
+
+;; Unbind arrow keys to force me to figure out emacs navigation
+(global-set-key (kbd "<up>") 'ignore)
+(global-set-key (kbd "<down>") 'ignore)
+(global-set-key (kbd "<left>") 'ignore)
+(global-set-key (kbd "<right>") 'ignore)
+(global-set-key (kbd "C-<up>") 'ignore)
+(global-set-key (kbd "C-<down>") 'ignore)
+(global-set-key (kbd "C-<left>") 'ignore)
+(global-set-key (kbd "C-<right>") 'ignore)
+(global-set-key (kbd "<home>") 'ignore)
+(global-set-key (kbd "<end>") 'ignore)
