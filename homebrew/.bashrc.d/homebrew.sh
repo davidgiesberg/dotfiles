72cc79b10c0d5727ed96576b3a2fd787c96ccf19
David Giesberg <git@davidgiesberg.net>
2015-08-30 11:48:45 -0500

    Homebrew bash-completion updates

--- homebrew/.bashrc.d/homebrew.sh
+++ homebrew/.bashrc.d/homebrew.sh
@@ -1,6 +1,11 @@
 # Make sure that homebrew versions of apps (like git) override builtin versions
 export PATH=/usr/local/bin:$PATH
 
-if [ -f $(brew --prefix)/etc/bash_completion ]; then
-  . $(brew --prefix)/etc/bash_completion
+# if [ -f $(brew --prefix)/etc/bash_completion ]; then
+#   set -x
+#   . $(brew --prefix)/etc/bash_completion
+#   set +x
+# fi
+if [ -f $(brew --prefix)/share/bash-completion/bash_completion ]; then
+  . $(brew --prefix)/share/bash-completion/bash_completion
 fi
