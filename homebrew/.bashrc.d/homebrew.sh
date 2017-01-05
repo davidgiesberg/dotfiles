1cef796a79eaab8707987ab9ecf193258582a2e3
David Giesberg <git@davidgiesberg.net>
2017-01-05 15:54:38 -0600

    Fix bash-completion initialization

--- homebrew/.bashrc.d/homebrew.sh
+++ homebrew/.bashrc.d/homebrew.sh
@@ -1,12 +1,11 @@
 # Make sure that homebrew versions of apps (like git) override builtin versions
 export PATH=/usr/local/bin:$PATH
 
-# if [ -f $(brew --prefix)/etc/bash_completion ]; then
-#   set -x
-#   . $(brew --prefix)/etc/bash_completion
-#   set +x
-# fi
-
-if [ -f $(brew --prefix)/share/bash-completion/bash_completion ]; then
-  . $(brew --prefix)/share/bash-completion/bash_completion
+# Make sure that you have the bash-completion homebrew package installed and updated (brew install bash-completion)
+if [ -f $(brew --prefix)/etc/bash_completion ]; then
+  . $(brew --prefix)/etc/bash_completion
 fi
+
+#if [ -f $(brew --prefix)/share/bash-completion/bash_completion ]; then
+#  . $(brew --prefix)/share/bash-completion/bash_completion
+#fi
