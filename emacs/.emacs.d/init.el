95f207df409f6c01fb028cf594c464718aed6022
David Giesberg <git@davidgiesberg.net>
2019-12-05 09:42:01 -0600

    Silence emacs load-path error

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -3,7 +3,7 @@
 
 (add-to-list 'load-path "~/.emacs.d/themes/emacs-color-theme-solarized")
 (add-to-list 'custom-theme-load-path "~/.emacs.d/themes/emacs-color-theme-solarized")
-(add-to-list 'load-path "~/.emacs.d/")
+;;(add-to-list 'load-path "~/.emacs.d/")
 
 (load-theme 'solarized-light t)
 
