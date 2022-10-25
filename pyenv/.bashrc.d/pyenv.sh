00a0def743b44df405fb6636ad3a2ca7907a826b
David Giesberg <giesberg.david@heb.com>
2022-10-25 16:47:27 -0500

    Other updates

--- pyenv/.bashrc.d/pyenv.sh
+++ pyenv/.bashrc.d/pyenv.sh
@@ -1,4 +1,6 @@
 if command -v pyenv 1>/dev/null 2>&1
 then
+    export PYENV_ROOT="$HOME/.pyenv"
+    command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"
     eval "$(pyenv init -)"
 fi
