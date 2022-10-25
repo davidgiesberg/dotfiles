bb102b25e6668501f11b195f5768c2b623f5f08b
David Giesberg <giesberg.david@heb.com>
2022-10-25 16:46:30 -0500

    Emacs updates

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -1,3 +1,9 @@
+;; use-package - https://github.com/jwiegley/use-package#getting-started
+(eval-when-compile
+  ;; Following line is not needed if use-package.el is in ~/.emacs.d
+  ;;(add-to-list 'load-path "<path where use-package is installed>")
+  (require 'use-package))
+
 ;;(require 'cask "~/.cask/cask.el")
 ;;(cask-initialize)
 
@@ -17,7 +23,7 @@
   show-trailing-whitespace t
 )
 
-(global-linum-mode t)
+(global-display-line-numbers-mode)
 (setq
    require-final-newline t
 )
@@ -34,12 +40,13 @@
 ;; (add-to-list 'auto-mode-alist '("\\.markdown\\'" . markdown-mode))
 ;; (add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
 
-(require 'package)
-(add-to-list 'package-archives
-             '("melpa-stable" . "https://stable.melpa.org/packages/"))
-(package-initialize)
+;; (require 'package)
+;; (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+;; (add-to-list 'package-archives
+;;              '("melpa-stable" . "https://stable.melpa.org/packages/"))
+;; (package-initialize)
 
-;; (use-package markdown-mode
+;; (Use-package markdown-mode
 ;;   :ensure t
 ;;   :commands (markdown-mode gfm-mode)
 ;;   :mode (("README\\.md\\'" . gfm-mode)
@@ -59,21 +66,88 @@
 (global-set-key (kbd "C-<right>") 'ignore)
 (global-set-key (kbd "<home>") 'ignore)
 (global-set-key (kbd "<end>") 'ignore)
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (markdown-mode))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
 
 (require 'yaml-mode)
 (add-to-list 'auto-mode-alist '("\\.yml\\'" . yaml-mode))
 
 (require 'terraform-mode)
 (add-to-list 'auto-mode-alist '("\\.tf\\'" . terraform-mode))
+
+;; ;; Auto completion
+(use-package company
+  :config
+  (setq company-idle-delay 0
+        company-minimum-prefix-length 4
+        company-selection-wrap-around t))
+(global-company-mode)
+
+;; git gutter
+;; (use-package git-gutter-fringe
+;;   :requires fringe-helper
+;;   :ensure)
+;; ;; TODO: Figure out if below require can be included in above use-package clause
+;; (require 'git-gutter-fringe)
+(require 'diff-hl)
+(global-diff-hl-mode)
+(unless (window-system) (diff-hl-margin-mode))
+;; (custom-set-variables
+;;  ;; custom-set-variables was added by Custom.
+;;  ;; If you edit it by hand, you could mess it up, so be careful.
+;;  ;; Your init file should contain only one such instance.
+;;  ;; If there is more than one, they won't work right.
+;;  '(package-selected-packages
+;;    '(helm ivy projectile cov python-coverage ag magit yaml-mode use-package terraform-mode diff-hl company)))
+;; (custom-set-faces
+;;  ;; custom-set-faces was added by Custom.
+;;  ;; If you edit it by hand, you could mess it up, so be careful.
+;;  ;; Your init file should contain only one such instance.
+;;  ;; If there is more than one, they won't work right.
+;;  )
+
+;; ag silver-searcher
+(use-package ag
+  :ensure t)
+
+;; python-coverage
+;;(use-package cov
+;;  :ensure)
+
+;; projectile (for managing/navigating projects)
+(use-package projectile
+  :ensure t
+  :init
+  (projectile-mode +1)
+  :bind (:map projectile-mode-map
+              ("C-c p" . projectile-command-map)))
+
+;; ;; Ivy completion
+;; (use-package ivy
+;;   :ensure t
+;;   :init
+;;   (ivy-mode +1))
+
+;; Straight package manager
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+      (bootstrap-version 5))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
+;;;;  Effectively replace use-package with straight-use-package
+;;; https://github.com/raxod502/straight.el/blob/develop/README.md#integration-with-use-package
+(straight-use-package 'use-package)
+(setq straight-use-package-by-default t)
+
+;; Helm
+
+
+(use-package helm
+  :straight t
+  :ensure t)
+(use-package helm-projectile)
