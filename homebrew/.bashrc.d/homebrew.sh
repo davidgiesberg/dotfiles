e86edce26478addb47c961cecc843d9c45ed5fe0
David Giesberg <git@davidgiesberg.net>
2022-12-27 10:42:19 -0600

    Dec 2022 updates

--- homebrew/.bashrc.d/homebrew.sh
+++ homebrew/.bashrc.d/homebrew.sh
@@ -1,5 +1,6 @@
 # Make sure that homebrew versions of apps (like git) override builtin versions
 export PATH=/opt/homebrew/bin:$PATH
+#eval "$(/opt/homebrew/bin/brew shellenv)"
 
 # Compatible with homebrew bash-completion@2
 export BASH_COMPLETION_COMPAT_DIR="$(brew --prefix)/etc/bash_completion.d"
