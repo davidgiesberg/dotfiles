71d357585a80e6862041993ec0da05f351b7584c
David Giesberg <giesberg.david@heb.com>
2025-05-22 09:29:25 -0500

    Temp - disabling pyenv to try uv

--- pyenv/.bashrc.d/pyenv.sh
+++ pyenv/.bashrc.d/pyenv.sh
@@ -1,6 +1,8 @@
+false # 2024-12-03 Disabling to try uv
 if command -v pyenv 1>/dev/null 2>&1
 then
     export PYENV_ROOT="$HOME/.pyenv"
     command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"
     eval "$(pyenv init -)"
+    eval "$(pyenv virtualenv-init -)"
 fi
